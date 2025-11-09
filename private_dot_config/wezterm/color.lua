20621c1b02d738b778b9a4a58bc370c408c36e93
ogadra <61941819+ogadra@users.noreply.github.com>
2025-11-10 02:34:42 +0900

    style: change color schema

--- private_dot_config/wezterm/color.lua
+++ private_dot_config/wezterm/color.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local module = {}
 
 function module.apply_to_config(config)
-    config.color_scheme = 'Material Darker (base16)'
+    config.color_scheme = 'Ef-Elea-Dark'
 end
 
 return module
