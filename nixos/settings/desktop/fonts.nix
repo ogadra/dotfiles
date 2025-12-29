e8c6f30f3b1490745825b077dc8c32b1a11b7fa2
ogadra <61941819+ogadra@users.noreply.github.com>
2025-12-29 23:51:49 +0900

    config: add japanese font (Noto CJK family)

---
+++ nixos/settings/desktop/fonts.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+      nerd-fonts.jetbrains-mono
+    ];
+
+    fontDir.enable = true;
+
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Noto Serif CJK JP"
+          "Noto Color Emoji"
+        ];
+        sansSerif = [
+          "Noto Sans CJK JP"
+          "Noto Color Emoji"
+        ];
+        monospace = [
+          "JetBrainMono Nerd Font"
+          "Noto Color Emoji"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
+}
--- profiles/bisharp/default.nix
+++ profiles/bisharp/default.nix
@@ -1,7 +1,14 @@
 { pkgs, ... }:
+let
+  # Desktop
+  desktopSettings = [
+    ../../nixos/settings/desktop/fonts.nix
+  ];
+in
 {
   networking.hostName = "bisharp";
   imports = [
     ./hardware-configuration.nix
-  ];
+  ]
+  ++ desktopSettings;
 }
