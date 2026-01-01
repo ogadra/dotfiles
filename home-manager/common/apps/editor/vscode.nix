67665eb0eec902e693b4493ef18675e46019140b
ogadra <61941819+ogadra@users.noreply.github.com>
2026-01-01 20:51:25 +0900

    settings: add vscode settings

--- home-manager/common/apps/editor/vscode.nix
+++ home-manager/common/apps/editor/vscode.nix
@@ -3,6 +3,19 @@
     programs = {
       vscode = {
         enable = true;
+        profiles.default = {
+            userSettings = {
+                "editor.fontFamily"            = "'CodeNewRoman Nerd Font Mono', 'Droid Sans Mono', 'monospace', monospace";
+                "editor.fontLigatures"         = false;
+                "editor.formatOnSave"          = true;
+                "editor.inlineSuggest.enabled" = true;
+
+                "explorer.confirmDelete" = false;
+                
+                "files.autoSave"           = "afterDelay";
+                "files.insertFinalNewline" = true;
+            };
+        };
       };
     };
 }
