a3e66674d28c142c078efcfc2886a0febce58305
ogadra <61941819+ogadra@users.noreply.github.com>
2026-01-04 20:05:36 +0900

    package: add fzf to nixos

--- home-manager/profiles/bisharp/default.nix
+++ home-manager/profiles/bisharp/default.nix
@@ -11,6 +11,7 @@ let
     ../../common/cli/gnumake
     ../../common/cli/claude-code
     ../../common/cli/fish
+    ../../common/cli/fzf
     ../../common/cli/mpv
     ../../common/cli/starship
   ];
