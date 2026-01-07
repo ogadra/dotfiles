14b773cfbd54973824c73e17dc9f1c2a96e319b1
ogadra <61941819+ogadra@users.noreply.github.com>
2026-01-07 09:59:43 +0900

    wip: use nix on darwin

---
+++ darwin/default.nix
@@ -0,0 +1,42 @@
+{
+  inputs,
+  profile,
+  system,
+  username,
+  ...
+}:
+let
+  baseModules = [
+    ./configuration.nix
+    ../profiles/${profile}
+  ];
+
+  homeManagerModules = [
+    inputs.home-manager.darwinModules.home-manager
+    (import ../home-manager/default.nix {
+      inherit
+        profile
+        username
+        inputs
+        ;
+    })
+  ];
+
+  allModules =
+    baseModules
+    ++ homeManagerModules
+    ;
+in
+{
+  inherit system;
+
+  specialArgs = {
+    inherit
+      inputs
+      profile
+      username
+      ;
+  };
+
+  modules = allModules;
+}
