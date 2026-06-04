973e56f66ab855a08a03374bad6b48e86e666be2
ogadra <61941819+ogadra@users.noreply.github.com>
2026-06-04 11:17:23 +0900

    fix(fish): repair ghq-fzf preview to run under zsh
    The preview used fish command substitution `(ghq root)`, but fzf runs
    the preview via $SHELL (zsh), where it became a glob and failed with
    `no matches found`. Pass the repo path as a positional arg to `sh -c`,
    use POSIX `$(ghq root)`, and discard errors so READMEs render.

--- home-manager/common/cli/fish/conf.d/functions.nix
+++ home-manager/common/cli/fish/conf.d/functions.nix
@@ -2,7 +2,7 @@
 {
   programs.fish.interactiveShellInit = ''
     function ghq-fzf
-      set src (ghq list | fzf --preview "bat --color=always --style=header,grid --line-range :80 (ghq root)/{}/README.*")
+      set src (ghq list | fzf --preview "sh -c 'bat --color=always --style=header,grid --line-range :80 \$(ghq root)/\$1/README.* 2>/dev/null' _ {}")
       if test -n "$src"
         cd (ghq root)/$src
       end
